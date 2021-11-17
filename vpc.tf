4def505b7c0cfc592b1ed578fe973dded4de6741
Rafael Falcao <rafael.falcao1@gmail.com>
2021-11-17 16:46:15 +0000

    added the rest of the subnets

--- vpc.tf
+++ vpc.tf
@@ -21,6 +21,42 @@ resource "aws_subnet" "dev-subnet-public-1" {
     }
 }
 
+resource "aws_subnet" "dev-subnet-public-2" {
+    vpc_id = "${aws_vpc.dev-vpc.id}"
+    count = lenght(data.aws_availability_zones.available.names)
+    cidr_block = "10.0.2.0/24"
+    map_public_ip_on_launch = "true" #public subnet
+    availability_zone = data.aws_availability_zones.available.names[count.index]
+
+    tags {
+        Name = "dev-subnet-public-12"
+    }
+}
+
+resource "aws_subnet" "dev-subnet-private-1" {
+    vpc_id = "${aws_vpc.dev-vpc.id}"
+    count = lenght(data.aws_availability_zones.available.names)
+    cidr_block = "10.0.3.0/24"
+    map_public_ip_on_launch = "false" 
+    availability_zone = data.aws_availability_zones.available.names[count.index]
+
+    tags {
+        Name = "dev-subnet-private-1"
+    }
+}
+
+resource "aws_subnet" "dev-subnet-private-2" {
+    vpc_id = "${aws_vpc.dev-vpc.id}"
+    count = lenght(data.aws_availability_zones.available.names)
+    cidr_block = "10.0.4.0/24"
+    map_public_ip_on_launch = "false" 
+    availability_zone = data.aws_availability_zones.available.names[count.index]
+
+    tags {
+        Name = "dev-subnet-private-2"
+    }
+}
+
 data "aws_availability_zones" "available" {
     state="available"
 }
