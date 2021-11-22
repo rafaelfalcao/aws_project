3efb07a46db54aec247a7e93f9c03d64b57b70a2
Rafael Falcao <rafael.falcao1@gmail.com>
2021-11-22 16:48:34 +0000

    added the code to push docker images to ecr

--- ecr-repo.tf
+++ ecr-repo.tf
@@ -3,7 +3,7 @@ resource "aws_ecr_repository" "ecr_repo" {
 }
 
 resource "aws_ecr_repository_policy" "repo-policy" {
-  repository = aws_ecr_repository.my_ecr_repo.name
+  repository = aws_ecr_repository.ecr_repo.name
   policy     = <<EOF
   {
     "Version": "2008-10-17",
@@ -29,12 +29,15 @@ resource "aws_ecr_repository_policy" "repo-policy" {
 }
 
 ##  Build Docker image and push to ECR
+
+#Reads the image metadata from a Docker Registry. Used in conjunction with the docker_image resource to keep an image up to date on the latest available version of the tag.
 resource "docker_registry_image" "frontend" {
-  name = "${aws_ecr_repository.ecr-repo.repository_url}:latest"
+  name = "${aws_ecr_repository.ecr_repo.repository_url}:latest"
 
   build { 
-    context = "../weather-app-indicator"
+    context = "weather-app-indicator"
     dockerfile = "Dockerfile"
   }  
 }
 
+ 
--- provider.tf
+++ provider.tf
@@ -2,12 +2,19 @@ provider "aws" {
     region = "${var.AWS_REGION}"
 }
 
-
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.15.0"
+    }
+  }
+}
 ## Setup Docker provider and proper credentials to push to ECR
 provider "docker" {
     registry_auth {
         address = local.aws_ecr_url
-        username = data.aws_ecr_authorization_token.dockertoken.username
+        username = data.aws_ecr_authorization_token.dockertoken.user_name
         password = data.aws_ecr_authorization_token.dockertoken.password
     }
   
