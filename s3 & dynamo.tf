8e56e7d4083bcae1ab6d0a32b8a81f1e53a897f1
Rafael Falcao <rafael.falcao1@gmail.com>
2021-11-29 00:12:26 +0000

    added s3, dynamo for shared state

---
+++ s3 & dynamo.tf
@@ -0,0 +1,23 @@
+resource "aws_s3_bucket" "bucket" {
+  bucket = "terraform-state-file-repo-rf"
+}
+
+
+terraform {
+  backend "s3" {
+    bucket = "terraformbackend"
+    key = "terraform"
+    region = "eu-west-2"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name           = "terraform-lock"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
