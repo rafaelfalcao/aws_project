6eabeaa0b9043c07811dfcefffedc9292eddb8a1
Rafael Falcao <rafael.falcao1@gmail.com>
2021-12-16 17:00:24 +0000

    changes on codebuild iam

--- ecr-repo.tf
+++ ecr-repo.tf
@@ -2,8 +2,6 @@ resource "aws_ecr_repository" "ecr_repo" {
     name  = "frontend-image"
 }
 
-
-
 ##  Build Docker image and push to ECR
 
 #Reads the image metadata from a Docker Registry. Used in conjunction with the docker_image resource to keep an image up to date on the latest available version of the tag.
